c68e739b1049b3e7668129a3799dff183f19e622
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-27 19:45:13 +0530

    Scope

--- swift-playground.playground/Pages/Scope.xcplaygroundpage/Contents.swift
+++ swift-playground.playground/Pages/Scope.xcplaygroundpage/Contents.swift
@@ -2,6 +2,22 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// using variables and constants of different scopes
+var multiplier = 1.2
+var total = 5.0
 
-//: [Next](@next)
+func first() {
+    let base = 10.0
+    total += base * multiplier
+}
+
+func second() {
+    let multiper = 5.0
+    let base = 3.5
+    total += base * multiper
+}
+
+first()
+second()
+
+print("Total \(total)")
